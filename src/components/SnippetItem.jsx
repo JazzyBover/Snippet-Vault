b6e8ec4620659e04b39e1b383b6b23379aadc472
neenu <rneenub@gmail.com>
2026-05-15 23:09:49 +0530

    button fixed

--- src/components/SnippetItem.jsx
+++ src/components/SnippetItem.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 
-const SnippetItem = ({ item, onDelete, setSelectedSnippet  }) => {
-  return (
-    <div className="snippet-item"
-      onClick={() => setSelectedSnippet(item)}>
-
-      <div className="snippet-icon">
-        {item.category === "React" && "⚛"}
-        {item.category === "JavaScript" && "JS"}
-        {item.category === "HTML" && "</>"}
-        {item.category === "CSS" && "#"}
-        </div>  
-        
-      <div className="snippet-content">
-        <h3>{item.title}</h3>
-        <p>{item.category}</p>
-
-</div>
-      <button className="delete-btn" onClick={onDelete}> ✕ </button>
-      
-    </div>
-  )
+const icons = {
+  React: "⚛",
+  JavaScript: "JS",
+  HTML: "</>",
+  CSS: "#"
 }
+
+const SnippetItem = ({ item, onDelete, setSelectedSnippet }) => (
+  
+  <div className="snippet-item">
+
+    <div className="snippet-icon">
+      {icons[item.category]}
+    </div>
+
+    <div className="snippet-content" onClick={() => setSelectedSnippet(item)}>
+      <h3>{item.title}</h3>
+      <p>{item.category}</p>
+    </div>      
+     <button className="delete-btn" onClick={onDelete}> ✕ </button>
+    </div>
+    )
 
 export default SnippetItem;
